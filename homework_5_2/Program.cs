8bc39b6346556794329e89863d10167a8604eb9d
Maria <mgashenina@gmail.com>
2022-12-16 22:48:43 +0300

    homework_5_2

---
+++ homework_5_2/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0 */
+
+
+void FillArray (int [] array)
+{
+    Random rnd = new Random();
+    for(int i = 0; i < array.Length; i++)
+    {
+        array [i] = rnd.Next(-99, 100);
+    }
+}
+
+void PrintArray (int [] array)
+{
+    for(int i=0; i < 4; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+int [] arr = new int[4];
+
+FillArray(arr);
+PrintArray (arr);
+
+int sum = 0;
+for(int i = 0; i < 4; i++)
+{
+    if (0 != i % 2)
+    {
+        sum = sum + arr [i];
+    }
+}
+Console.WriteLine();
+Console.WriteLine(sum);
